9f8e981e5bf728d65452de33680670714f37f53e
Robert Barrios <ryba.9125@gmail.com>
2021-11-15 14:57:51 -0500

    Seteo de primeros botones, clase operaciones y método suma

---
+++ functionsCalculator/functions.py
@@ -0,0 +1,40 @@
+# from setButtonsCalculator import screenNumber, result
+from tkinter import StringVar
+
+class Functions():
+
+    def __init__(self):
+        self.operation:str = ""
+        self.result = 0
+        self.screenNumber = StringVar()
+
+    #----------------------------pulsaciones teclado-----------
+
+    def numeroPulsado(self, num: str):
+        global operation
+
+        if self.operation != "":
+            self.screenNumber.set(num)
+            operation = ""
+        else:
+            self.screenNumber.set(self.screenNumber.get() + num)
+            
+    #-----------------------función suma-----------------------
+    def suma(self, num):
+        # global operation
+        # global result
+        self.result += int(num)
+        self.operation = "suma"
+        self.screenNumber.set(self.result)
+
+    #-------- sustraction method-----------
+    def sustraction(self, num):
+        self.result -= int(num)
+        self.operation = "sustraction"
+
+    #------------Función el_resultado----------------------------
+    def el_resultado(self):
+        global result
+        self.screenNumber.set(self.result + int(self.screenNumber.get()))
+
+        self.result = 0
