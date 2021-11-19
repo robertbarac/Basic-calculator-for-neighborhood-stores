d181c742dc8b3ecb58bdec118effeb84bd13798d
Robert Barrios <ryba.9125@gmail.com>
2021-11-18 22:47:47 -0500

    remaking the calculator from the base, commenting the class methods, adding the remaining buttons for the basic calculator

--- functionsCalculator/functions.py
+++ functionsCalculator/functions.py
@@ -1,4 +1,3 @@
-# from setButtonsCalculator import screenNumber, result
 from tkinter import StringVar
 
 class Functions():
@@ -7,84 +6,127 @@ class Functions():
         self.operation:str = ""
         self.result = 0
         self.screenNumber = StringVar()
+        #self.position = 0
 
     #----------------------------key pressed method-----------
     def number_pressed(self, num: str):
-        if self.operation != "":
-            self.screenNumber.set(num)
-            if self.result == 0:
-                self.operation = ""
-        else:
-            self.screenNumber.set(self.screenNumber.get() + num)
+        pass
+        # if self.operation != "":
+        #     self.screenNumber.set(num)
+        #     if self.result == 0:
+        #         self.operation = ""
+        # else:
+        #     self.screenNumber.set(self.screenNumber.get() + num)
 
     #-----------------------addition method-----------------------
     def addition(self, num):
-        if self.result != 0:
-            self.acumulated_result(num)
-        else:
-            if type(self.result) == 'int':
-                self.result += int(num)
-            else:
-                self.result += float(num)
-        self.operation = "addition"
-        self.screenNumber.set(self.result)
+        # if self.result != 0:
+        #     self.acumulated_result(num)
+        # else:
+        #     if self.screen_number_is_int():
+        #         self.result += float(self.screenNumber.get())
+        #     else:
+        #         self.result += int(self.screenNumber.get())
+        # self.operation = "addition"
+        pass
 
     #-----------------------sustraction method-----------------
     def subtraction(self, num):
-        if self.result != 0:
-            self.acumulated_result(num)
-        else:
-            self.result += int(num)
-        self.operation = "subtraction"
-        self.screenNumber.set(self.result)
+        # if self.result != 0:
+        #     self.acumulated_result(num)
+        # else:
+        #     if self.screen_number_is_int():
+        #         self.result += float(self.screenNumber.get())
+        #     else:
+        #         self.result += int(self.screenNumber.get())
+        # self.operation = "subtraction"
+        pass
 
     #-----------------------multiplication method--------------
     def multiplication(self, num):
-        if self.result != 0:
-            self.acumulated_result(num)
-        else:
-            self.result += int(num)
-        self.operation = "multiplication"
-        self.screenNumber.set(self.result)
+        # if self.result != 0:
+        #     self.acumulated_result(num)
+        # else:
+        #     if self.screen_number_is_int():
+        #         self.result += float(self.screenNumber.get())
+        #     else:
+        #         self.result += int(self.screenNumber.get())
+        # self.operation = "multiplication"
+        pass
 
     #-----------------------division method--------------------
     def division(self, num):
-        if self.result != 0:
-            self.acumulated_result(num)
-        else:
-            self.result += int(num)
-        self.operation = "division"
-        self.screenNumber.set(self.result)
-    
+        # if self.result != 0:
+        #     self.acumulated_result(num)
+        # else:
+        #     if self.screen_number_is_int():
+        #         self.result += float(self.screenNumber.get())
+        #     else:
+        #         self.result += int(self.screenNumber.get())
+        # self.operation = "division"
+        pass
     #----------------------result acumulated method-------------
     def acumulated_result(self, num):
-        if self.operation == "addition":
-            if self.is_int():
-                self.result += int(num)
-            else:
-                self.result += float(num)
-        elif self.operation == "subtraction":
-            self.result -= int(num)
-            #self.screenNumber.get()
-        elif self.operation == "multiplication":
-            self.result *= int(num)
-        elif self.operation == "division":
-            self.result /= int(num)
+        # if self.operation == "addition":
+        #     if self.is_int():
+        #         self.result += int(num)
+        #     else:
+        #         self.result += float(num)
+        # elif self.operation == "subtraction":
+        #     self.result -= int(num)
+        #     #self.screenNumber.get()
+        # elif self.operation == "multiplication":
+        #     self.result *= float(num)
+        # elif self.operation == "division":
+        #     self.result /= float(num)
+        pass
 
     #----------------------the_result method------------------
     def the_result(self):
-        if self.operation == "addition":
-            self.screenNumber.set(self.result + int(self.screenNumber.get()))
-        elif self.operation == "subtraction":
-            self.screenNumber.set(self.result - int(self.screenNumber.get()))
-        elif self.operation == "multiplication":
-            self.screenNumber.set(self.result * int(self.screenNumber.get()))
-        elif self.operation == "division":
-            if self.result % int(self.screenNumber.get()) == 0:
-                self.screenNumber.set(int(self.result / int(self.screenNumber.get())))
-            else:
-                self.screenNumber.set(self.result / int(self.screenNumber.get()))
-        self.result = 0
+        # if self.operation == "addition":
+        #     self.result += float(self.screenNumber.get())
+        #     if len(str(self.result)[str(self.result).find('.'):len(str(self.result)) + 1]) == 2 and (str(self.result)[str(self.result).find('.') + 1] == '0'):
+        #         self.screenNumber.set(round(self.result))
+        #     else:
+        #         self.screenNumber.set(self.result)
+            
+        # elif self.operation == "subtraction":
+        #     self.result -= float(self.screenNumber.get())
+        #     if len(str(self.result)[str(self.result).find('.'):len(str(self.result)) + 1]) == 2 and (str(self.result)[str(self.result).find('.') + 1] == '0'):
+        #         self.screenNumber.set(round(self.result))
+        #     else:
+        #         self.screenNumber.set(self.result)
+        # elif self.operation == "multiplication":
+        #     self.result = self.result * float(self.result * float(self.screenNumber.get()))
+        #     if len(str(self.result)[str(self.result).find('.'):len(str(self.result)) + 1]) == 2 and (str(self.result)[str(self.result).find('.') + 1] == '0'):
+        #         self.screenNumber.set(round(self.result))
+        #     else:
+        #         self.screenNumber.set(self.result)
+            
+        # elif self.operation == "division":
+        #     self.result /= float(self.screenNumber.get())
+        #     if len(str(self.result)[str(self.result).find('.'):len(str(self.result)) + 1]) == 2 and (str(self.result)[str(self.result).find('.') + 1] == '0'):
+        #         self.screenNumber.set(round(self.result))
+        #     else:
+        #         self.screenNumber.set(self.result)
+        # #self.result = 0
+        # self.operation = ''
+        pass
 
-    def is_int(self):
-        return True if type(self.result) == 'int' else 'float'
+    def result_is_int(self):
+        # return True if type(self.result) == 'int' else False
+        pass
+
+    def screen_number_is_int(self):
+        # return True if '.' in str(self.screenNumber.get()) else False
+        pass
+
+    def clear_screen(self):
+        # self.screenNumber.set('0')
+        pass
+
+    def clear_all(self):
+        # self.screenNumber.set('0')
+        # self.result = 0
+        # self.operation = ''
+        pass
--- setButtonsCalculator.py
+++ setButtonsCalculator.py
@@ -3,6 +3,8 @@ from typing import Collection
 from functionsCalculator.functions import Functions
 
 root = Tk()
+root.title("Basic Calculator")
+root.iconbitmap("perfil.ico")
 myFrame = Frame(root)
 
 myFrame.pack()
@@ -14,9 +16,71 @@ functions_pack = Functions()
 # # --------------------Screen--------------------------
 # screenNumber = StringVar()
 screen = Entry(myFrame, textvariable=functions_pack.screenNumber)
-screen.grid(row=1, column=1, padx=5, pady=5, columnspan=4)
+screen.grid(row=0, column=1, padx=5, pady=5, columnspan=6, sticky= W + E)
 
 screen.config(background="black", fg="#03f943", justify="right")
+
+#-------------------Row 1----------------------------------
+
+mor_less_button = Button(myFrame, text= '+/-', pady=10, width=3)
+mor_less_button.grid(row=2, column=1)
+button_7 = Button(myFrame, text="7", pady=10, width=3, command=lambda:functions_pack.number_pressed("7"))
+button_7.grid(row=2, column=2)
+button_8 = Button(myFrame, text="8", pady=10, width=3, command=lambda:functions_pack.number_pressed("8"))
+button_8.grid(row=2, column=3)
+button_9 = Button(myFrame, text="9", pady=10, width=3, command=lambda:functions_pack.number_pressed("9"))
+button_9.grid(row=2, column=4)
+percentage_button = Button(myFrame, text="%", pady=10, width=3)
+percentage_button.grid(row=2, column=5)
+square_root_button = Button(myFrame, text="√", pady=10, width=3)
+square_root_button.grid(row=2, column=6)
+
+
+#-------------------Row 2----------------------------------
+
+arrow_button = Button(myFrame, text="◀", pady=10, width=3)
+arrow_button.grid(row=3, column=1)
+button_4 = Button(myFrame, text="4", pady=10, width=3, command=lambda:functions_pack.number_pressed("4"))
+button_4.grid(row=3, column=2)
+button_5 = Button(myFrame, text="5", pady=10, width=3, command=lambda:functions_pack.number_pressed("5"))
+button_5.grid(row=3, column=3)
+button_6 = Button(myFrame, text="6", pady=10, width=3, command=lambda:functions_pack.number_pressed("6"))
+button_6.grid(row=3, column=4)
+button_mult = Button(myFrame, text="X", pady=10, width=3, command=lambda:functions_pack.multiplication(functions_pack.screenNumber.get()))
+button_mult.grid(row=3, column=5)
+button_div = Button(myFrame, text="÷", pady=10, width=3, command=lambda:functions_pack.division(functions_pack.screenNumber.get()))
+button_div.grid(row=3, column=6)
+
+#-------------------Row 3----------------------------------
+
+clear_button = Button(myFrame, text="C", pady=10, width=3)
+clear_button.grid(row=4, column=1)
+button_1 = Button(myFrame, text="1", pady=10, width=3, command=lambda:functions_pack.number_pressed("1"))
+button_1.grid(row=4, column=2)
+button_2 = Button(myFrame, text="2", pady=10, width=3, command=lambda:functions_pack.number_pressed("2"))
+button_2.grid(row=4, column=3)
+button_3 = Button(myFrame, text="3", pady=10, width=3, command=lambda:functions_pack.number_pressed("3"))
+button_3.grid(row=4, column=4)
+button_rest = Button(myFrame, text="-", pady=10, width=3, command=lambda:functions_pack.subtraction(functions_pack.screenNumber.get()))
+button_rest.grid(row=4, column=6)
+
+#---------------------Row 4---------------------------------
+
+ac_button = Button(myFrame, text="AC", pady=10, width=3)
+ac_button.grid(row=5, column=1)
+button_0 = Button(myFrame, text="0", pady=10, width=3, command=lambda:functions_pack.number_pressed("0"))
+button_0.grid(row=5, column=2)
+button_double0 = Button(myFrame, text="00", pady=10, width=3)
+button_double0.grid(row=5, column=3)
+button_dot = Button(myFrame, text=".", pady=10, width=3, command=lambda:functions_pack.number_pressed('.'))
+button_dot.grid(row=5, column=4)
+button_equals = Button(myFrame, text="=", pady=10, width=3, command=lambda:functions_pack.the_result())
+button_equals.grid(row=5, column=6)
+button_sum = Button(myFrame, text="+", pady=30, width=3, command=lambda:functions_pack.addition(functions_pack.screenNumber.get()))
+button_sum.grid(row=4, column=5, rowspan=2)
+
+if __name__ == "__main__":
+    root.mainloop()
 
 """#----------------------------pulsaciones teclado-----------
 
@@ -44,56 +108,3 @@ def el_resultado():
 
     result = 0
 """
-
-#-------------------Row 1----------------------------------
-
-button_7 = Button(myFrame, text="7", pady=10, width=3, command=lambda:functions_pack.number_pressed("7"))
-button_7.grid(row=2, column=1)
-button_8 = Button(myFrame, text="8", pady=10, width=3, command=lambda:functions_pack.number_pressed("8"))
-button_8.grid(row=2, column=2)
-button_9 = Button(myFrame, text="9", pady=10, width=3, command=lambda:functions_pack.number_pressed("9"))
-button_9.grid(row=2, column=3)
-button_div = Button(myFrame, text="÷", pady=10, width=3, command=lambda:functions_pack.division(functions_pack.screenNumber.get()))
-button_div.grid(row=2, column=4)
-
-#-------------------Row 2----------------------------------
-
-button_4 = Button(myFrame, text="4", pady=10, width=3, command=lambda:functions_pack.number_pressed("4"))
-button_4.grid(row=3, column=1)
-button_5 = Button(myFrame, text="5", pady=10, width=3, command=lambda:functions_pack.number_pressed("5"))
-button_5.grid(row=3, column=2)
-button_6 = Button(myFrame, text="6", pady=10, width=3, command=lambda:functions_pack.number_pressed("6"))
-button_6.grid(row=3, column=3)
-button_mult = Button(myFrame, text="X", pady=10, width=3, command=lambda:functions_pack.multiplication(functions_pack.screenNumber.get()))
-button_mult.grid(row=3, column=4)
-
-#-------------------Row 3----------------------------------
-
-button_1 = Button(myFrame, text="1", pady=10, width=3, command=lambda:functions_pack.number_pressed("1"))
-button_1.grid(row=4, column=1)
-button_2 = Button(myFrame, text="2", pady=10, width=3, command=lambda:functions_pack.number_pressed("2"))
-button_2.grid(row=4, column=2)
-button_3 = Button(myFrame, text="3", pady=10, width=3, command=lambda:functions_pack.number_pressed("3"))
-button_3.grid(row=4, column=3)
-button_rest = Button(myFrame, text="-", pady=10, width=3, command=lambda:functions_pack.subtraction(functions_pack.screenNumber.get()))
-button_rest.grid(row=4, column=4)
-
-#---------------------Row 4---------------------------------
-
-button_dot = Button(myFrame, text=".", pady=10, width=3)
-button_dot.grid(row=5, column=1)
-button_0 = Button(myFrame, text="0", pady=10, width=3, command=lambda:functions_pack.number_pressed("0"))
-button_0.grid(row=5, column=2)
-button_equals = Button(myFrame, text="=", pady=10, width=3, command=lambda:functions_pack.the_result())
-button_equals.grid(row=5, column=3)
-button_sum = Button(myFrame, text="+", pady=10, width=3, command=lambda:functions_pack.addition(functions_pack.screenNumber.get()))
-button_sum.grid(row=5, column=4)
-
-#------------------------Row 5-----------------------------
-#button
-
-
-
-
-if __name__ == "__main__":
-    root.mainloop()
